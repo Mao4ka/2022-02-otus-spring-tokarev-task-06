c4f77ea272a05f4c101e593929fc9bf58b0bfce9
tokarev-aa <atokarev2@list.ru>
2022-06-18 07:30:23 +0300

    task-06 code clear

--- src/main/java/ru/otus/entity/Author.java
+++ src/main/java/ru/otus/entity/Author.java
@@ -3,7 +3,6 @@ package ru.otus.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.CascadeType;
